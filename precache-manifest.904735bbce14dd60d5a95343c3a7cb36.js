9a50ba3959f44962fa7018733e4b83a52dacb82d
Seyed Farzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-08-04 01:19:48 +0430

    Updates

---
+++ precache-manifest.904735bbce14dd60d5a95343c3a7cb36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eca4a1865a1f87bfc0e",
+    "url": "/jobsurvey/static/css/main.3665b53e.chunk.css"
+  },
+  {
+    "revision": "5eca4a1865a1f87bfc0e",
+    "url": "/jobsurvey/static/js/main.5eca4a18.chunk.js"
+  },
+  {
+    "revision": "59837409e326479ad65d",
+    "url": "/jobsurvey/static/js/1.59837409.chunk.js"
+  },
+  {
+    "revision": "d3bba73cd4002df715f2",
+    "url": "/jobsurvey/static/js/runtime~main.d3bba73c.js"
+  },
+  {
+    "revision": "b3933bee06d9e18108058f5c0b29f03a",
+    "url": "/jobsurvey/index.html"
+  }
+];
